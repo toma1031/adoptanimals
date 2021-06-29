fec317e5e3f0308f0f3abba6b60e8ab570f7967e
Akinori Toma <akinoritoma@appletoma.home>
2021-06-29 03:39:21 -0400

    fix send message button post_detail.html

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animal_project.settings_mysql')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animal_project.settings_mysql_local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
